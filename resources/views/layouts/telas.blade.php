3642021b6cbed4da5c6f850e24d7917d5da01bd3
Vitor221 <vitornascimento6650@gmail.com>
2023-04-10 17:00:59 -0300

    commit: estilizando tabela clientes

--- resources/views/layouts/telas.blade.php
+++ resources/views/layouts/telas.blade.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-    <nav>
+    <nav class="nav">
         <img src="{{ asset('../img/Skedoo.png') }}" alt="">
         <h3>Bem-vindo, {{ session('login')['nm_login'] }}</h3>
         <x-profile-button />
--- resources/views/telas/clientes.blade.php
+++ resources/views/telas/clientes.blade.php
@@ -7,7 +7,6 @@
             <tr>
                 <th scope="col">Nome</th>
                 <th scope="col">Cadastro</th>
-                <th scope="col"></th>
             </tr>
         </thead>
         <tbody>
@@ -16,9 +15,8 @@
                     <th class="nome">{{ $TbResponsavel->nm_responsavel }}</th>
                     <th scope>{{ $TbResponsavel->cd_cadastro }}</th>
                     <th class="botoes"><button class="ver">Vizualizar</button>
-                    <button class="editar">Editar</button>
-                    <button class="deletar"><i class="uil uil-trash-alt"></i></button>
-                    </th>
+                    <th class="botoes"><button class="editar">Editar</button></th>
+                    <th class="botoes"><button class="deletar"><i class="uil uil-trash-alt"></i></button></th>
                 </tr>
             @endforeach
         </tbody>
