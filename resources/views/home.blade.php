e7bec71b7037ac6805ca328222bd67ee3c64a1b1
MatheusReis97 <Matheus.drs97@hotmail.com>
2023-04-11 19:52:46 -0300

    commit: "Adc Vlibras"
    Adcionado Vlibras nas paginas iniciais.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -125,5 +125,17 @@
       <img src="{{ asset('img/img_home/cinco_estrelas.png') }}" alt="">
     </div>
   </div>
+
+  {{-- VLibras --}}
+  <div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>
 </section>
 @endsection
