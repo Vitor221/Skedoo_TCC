9b8ee814e3384b094f377fd76fa6d3191d1cf732
pxrtess <pedrohenports@gmail.com>
2023-05-16 20:29:37 -0300

    estilização form refeição

---
+++ public/js/configRefeicao.js
@@ -0,0 +1,8 @@
+function formRefeicao(){
+    document.getElementById("form").style='display:auto';
+    document.getElementById("btnInsert").style='display:none';
+}
+function formRefeicaoFechar(){
+    document.getElementById("form").style='display:none';
+    document.getElementById("btnInsert").style='display:auto';
+}
