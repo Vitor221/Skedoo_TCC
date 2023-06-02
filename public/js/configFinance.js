93381d4ac119ff88ef1843213990718b765b2e3a
Fabricio Henrique <fabricio.fh086@gmail.com>
2023-06-02 16:48:16 -0300

    feat: testing pass data in chartjs

--- public/js/configFinance.js
+++ public/js/configFinance.js
@@ -1,16 +1,25 @@
+import Chart from 'chart.js/auto';
+
 const ctx = document.getElementById("myChart");
+
+const turmas = [
+  @foreach ($TbTurmas as $Turmas)
+  <tr>
+      <td class="nome">{{ $Turmas->nm_turma }}</td>
+      @if (isset($alunosPorTurma[$Turmas->cd_turma]))
+          <td>{{ $alunosPorTurma[$Turmas->cd_turma]->total_alunos }}</td>
+      @else
+          <td>0</td>
+      @endif
+      <td>{{ $Turmas->cd_total_aluno }}</td>
+  </tr>
+@endforeach
+]
 
 new Chart(ctx, {
     type: "doughnut",
     data: {
-        labels: [
-            "Berçário I",
-            "Berçário II",
-            "Maternal I",
-            "Maternal II",
-            "Fase I",
-            "Fase II",
-        ],
+        labels: turmas,
         datasets: [
             {
                 label: "Alunos por turma",
