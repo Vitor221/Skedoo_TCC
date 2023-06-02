5a488e5d054f144d1eb764d33cfe2f17833cf8f1
Fabricio Henrique <fabricio.fh086@gmail.com>
2023-06-02 16:10:37 -0300

    feat: financeiro passing to dashboard

--- public/js/configFinance.js
+++ public/js/configFinance.js
@@ -1,20 +1,29 @@
-const ctx = document.getElementById('myChart');
-  
-    new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: ['Berçário I', 'Berçário II', 'Maternal I', 'Maternal II', 'Fase I', 'Fase II'],
-        datasets: [{
-          label: 'Alunos por turma',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      },
-      options: {
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+    type: "doughnut",
+    data: {
+        labels: [
+            "Berçário I",
+            "Berçário II",
+            "Maternal I",
+            "Maternal II",
+            "Fase I",
+            "Fase II",
+        ],
+        datasets: [
+            {
+                label: "Alunos por turma",
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
         scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
