7d84344896b76e90e65d7639e78a73af0c3366ad
Fabricio Henrique <fabricio.fh086@gmail.com>
2023-06-02 22:48:25 -0300

    feat: preparar js p/ dados

--- public/js/configFinance.js
+++ public/js/configFinance.js
@@ -1,38 +1,30 @@
-import Chart from 'chart.js/auto';
+import Chart from "chart.js/auto";
 
-const ctx = document.getElementById("myChart");
+$.ajax({
+    url: "/dados-grafico",
+    method: "GET",
+    sucess: function (response) {
+        const ctx = document.getElementById("myChart");
 
-const turmas = [
-  @foreach ($TbTurmas as $Turmas)
-  <tr>
-      <td class="nome">{{ $Turmas->nm_turma }}</td>
-      @if (isset($alunosPorTurma[$Turmas->cd_turma]))
-          <td>{{ $alunosPorTurma[$Turmas->cd_turma]->total_alunos }}</td>
-      @else
-          <td>0</td>
-      @endif
-      <td>{{ $Turmas->cd_total_aluno }}</td>
-  </tr>
-@endforeach
-]
-
-new Chart(ctx, {
-    type: "doughnut",
-    data: {
-        labels: turmas,
-        datasets: [
-            {
-                label: "Alunos por turma",
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1,
+        new Chart(ctx, {
+            type: "doughnut",
+            data: {
+                labels: turmas,
+                datasets: [
+                    {
+                        label: "Alunos por turma",
+                        data: [12, 19, 3, 5, 2, 3],
+                        borderWidth: 1,
+                    },
+                ],
             },
-        ],
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
             },
-        },
+        });
     },
 });
